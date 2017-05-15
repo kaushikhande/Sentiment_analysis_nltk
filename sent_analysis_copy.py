0749226e57a58107638437798b00ff44e7510d60
kaushikhande <kaushikhande302@gmail.com>
2017-05-15 16:52:14 +0530

    Added new files sent_analysis_copy and sentiment_analysis_copy

---
+++ sent_analysis_copy.py
@@ -0,0 +1,253 @@
+import collections
+import nltk
+import csv
+import nltk.classify.util, nltk.metrics
+from sklearn import cross_validation
+from sklearn.svm import LinearSVC, SVC
+import random
+from nltk.corpus import stopwords
+import itertools
+from nltk.collocations import BigramCollocationFinder
+from nltk.collocations import TrigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+from nltk.metrics import TrigramAssocMeasures
+from nltk.sentiment.util import mark_negation
+from nltk.classify import NaiveBayesClassifier, MaxentClassifier, SklearnClassifier
+
+posdata = []
+#with open('positive-data.csv', 'rb') as myfile:    
+#    reader = csv.reader(myfile, delimiter=',')
+#    for val in reader:
+#        posdata.append(val[0])        
+ 
+negdata = []
+#with open('negative-data.csv', 'rb') as myfile:    
+#    reader = csv.reader(myfile, delimiter=',')
+#    for val in reader:
+#        negdata.append(val[0])            
+
+def load_file():
+    with open('review.csv') as csv_file:
+        reader = csv.reader(csv_file,delimiter=",",quotechar='"')
+        reader.next()
+        posdata = []
+        negdata = []
+        for row in reader:
+            # skip missing data
+            if row[0] and row[1]:
+                data_review_tuple = row[0]
+                
+                #target.append(row[1])
+                if row[1] == 'positive':
+                    posdata.append(data_review_tuple)
+                else:
+                    negdata.append(data_review_tuple)
+
+        return posdata,negdata
+
+posdata,negdata = load_file()
+#print posdata
+#print negdata
+
+def word_split(data):    
+    data_new = []
+    for word in data:
+        word_filter = [i.lower() for i in word.split()]
+        data_new.append(word_filter)
+    return data_new
+ 
+def word_split_sentiment(data):
+    data_new = []
+    for (word, sentiment) in data:
+        word_filter = [i.lower() for i in word.split()]
+        data_new.append((word_filter, sentiment))
+    return data_new
+    
+def word_feats(words):    
+    return dict([(word, True) for word in words])
+ 
+stopset = set(stopwords.words('english')) - set(('over', 'under', 'below', 'more', 'most', 'no', 'not', 'only', 'such', 'few', 'so', 'too', 'very', 'just', 'any', 'once'))
+     
+def stopword_filtered_word_feats(words):
+    return dict([(word, True) for word in words if word not in stopset])
+ 
+def bigram_word_feats(words, score_fn=BigramAssocMeasures.chi_sq, n=200):
+    bigram_finder = BigramCollocationFinder.from_words(words)
+    bigrams = bigram_finder.nbest(score_fn, n)
+    """
+    print words
+    for ngram in itertools.chain(words, bigrams): 
+        if ngram not in stopset: 
+            print ngram
+    exit()
+    """    
+    return dict([(ngram, True) for ngram in itertools.chain(words, bigrams)])
+    
+def trigram_word_feats(words, score_fn=TrigramAssocMeasures.chi_sq, n=50):
+    trigram_finder = TrigramCollocationFinder.from_words(words)
+    trigrams = trigram_finder.nbest(score_fn, n)
+    """
+    print words
+    for ngram in itertools.chain(words, bigrams): 
+        if ngram not in stopset: 
+            print ngram
+    exit()
+    """    
+    return dict([(ngram, True) for ngram in itertools.chain(words, trigrams)])
+    
+
+#def bigram_word_feats_stopwords(words, score_fn=BigramAssocMeasures.chi_sq, n=200):
+def bigram_word_feats_stopwords(words, score_fn=BigramAssocMeasures.mi_like, n=50):
+    bigram_finder = BigramCollocationFinder.from_words(words)
+    bigrams = bigram_finder.nbest(score_fn, n)
+    #print bigrams
+    """
+    print words
+    for ngram in itertools.chain(words, bigrams): 
+        if ngram not in stopset: 
+            print ngram
+    exit()
+    """    
+    return dict([(ngram, True) for ngram in itertools.chain(words, bigrams) if ngram not in stopset])
+ 
+# Calculating Precision, Recall & F-measure
+def evaluate_classifier(featx):
+    
+    #negfeats = [(featx(mark_negation(f)), 'neg') for f in word_split(negdata)]
+    #posfeats = [(featx(mark_negation(f)), 'pos') for f in word_split(posdata)]
+    negfeats = [(featx(f), 'neg') for f in word_split(negdata)]
+    #print negfeats
+    #raw_input('>')
+    posfeats = [(featx(f), 'pos') for f in word_split(posdata)]    
+    negcutoff = len(negfeats)*4/5
+    poscutoff = len(posfeats)*4/5
+ 
+    trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
+    print(len(trainfeats))
+    #print trainfeats
+    testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
+    print(len(testfeats))
+    
+    # using 3 classifiers
+    classifier_list = ['nb', 'svm', 'maxent']#     
+        
+    for cl in classifier_list:
+        if cl == 'maxent':
+            classifierName = 'Maximum Entropy'
+            classifier = MaxentClassifier.train(trainfeats, 'GIS', trace=0, encoding=None, labels=None, gaussian_prior_sigma=0, max_iter = 1)
+        elif cl == 'svm':
+            classifierName = 'SVM'
+            classifier = SklearnClassifier(LinearSVC(), sparse=False)
+            classifier.train(trainfeats)
+        else:
+            classifierName = 'Naive Bayes'
+            classifier = NaiveBayesClassifier.train(trainfeats)
+            
+        refsets = collections.defaultdict(set)
+        testsets = collections.defaultdict(set)
+ 
+        for i, (feats, label) in enumerate(testfeats):
+                refsets[label].add(i)
+                #print feats
+                #raw_input('> ')
+                observed = classifier.classify(feats)
+                testsets[observed].add(i)
+ 
+        #print refsets['pos']
+        #print testsets['pos']
+        accuracy = nltk.classify.util.accuracy(classifier, testfeats)
+        pos_precision = nltk.precision(refsets['pos'], testsets['pos'])
+        pos_recall = nltk.recall(refsets['pos'], testsets['pos'])
+        pos_fmeasure = nltk.f_measure(refsets['pos'], testsets['pos'])
+        neg_precision = nltk.precision(refsets['neg'], testsets['neg'])
+        neg_recall = nltk.recall(refsets['neg'], testsets['neg'])
+        neg_fmeasure =  nltk.f_measure(refsets['neg'], testsets['neg'])
+        
+        print('')
+        print('---------------------------------------')
+        print('SINGLE FOLD RESULT ' + '(' + classifierName + ')')
+        print('---------------------------------------')
+        print('accuracy:', accuracy)
+        print('precision', (pos_precision + neg_precision) / 2)
+        print('recall', (pos_recall + neg_recall) / 2)
+        print('f-measure', (pos_fmeasure + neg_fmeasure) / 2)    
+                
+        #classifier.show_most_informative_features()
+    
+    print('')
+    
+    ## CROSS VALIDATION
+    
+    trainfeats = negfeats + posfeats    
+    
+    # SHUFFLE TRAIN SET
+    # As in cross validation, the test chunk might have only negative or only positive data    
+    random.shuffle(trainfeats)    
+    n = 5 # 5-fold cross-validation    
+    
+    for cl in classifier_list:
+        
+        subset_size = len(trainfeats) / n
+        accuracy = []
+        pos_precision = []
+        pos_recall = []
+        neg_precision = []
+        neg_recall = []
+        pos_fmeasure = []
+        neg_fmeasure = []
+        cv_count = 1
+        for i in range(n):        
+            testing_this_round = trainfeats[i*subset_size:][:subset_size]
+            training_this_round = trainfeats[:i*subset_size] + trainfeats[(i+1)*subset_size:]
+            
+            if cl == 'maxent':
+                classifierName = 'Maximum Entropy'
+                classifier = MaxentClassifier.train(training_this_round, 'GIS', trace=0, encoding=None, labels=None, gaussian_prior_sigma=0, max_iter = 1)
+            elif cl == 'svm':
+                classifierName = 'SVM'
+                classifier = SklearnClassifier(LinearSVC(), sparse=False)
+                classifier.train(training_this_round)
+            else:
+                classifierName = 'Naive Bayes'
+                classifier = NaiveBayesClassifier.train(training_this_round)
+                    
+            refsets = collections.defaultdict(set)
+            testsets = collections.defaultdict(set)
+            for i, (feats, label) in enumerate(testing_this_round):
+                refsets[label].add(i)
+                observed = classifier.classify(feats)
+                testsets[observed].add(i)
+            
+            cv_accuracy = nltk.classify.util.accuracy(classifier, testing_this_round)
+            cv_pos_precision = nltk.precision(refsets['pos'], testsets['pos'])
+            cv_pos_recall = nltk.recall(refsets['pos'], testsets['pos'])
+            cv_pos_fmeasure = nltk.f_measure(refsets['pos'], testsets['pos'])
+            cv_neg_precision = nltk.precision(refsets['neg'], testsets['neg'])
+            cv_neg_recall = nltk.recall(refsets['neg'], testsets['neg'])
+            cv_neg_fmeasure =  nltk.f_measure(refsets['neg'], testsets['neg'])
+                    
+            accuracy.append(cv_accuracy)
+            pos_precision.append(cv_pos_precision)
+            pos_recall.append(cv_pos_recall)
+            neg_precision.append(cv_neg_precision)
+            neg_recall.append(cv_neg_recall)
+            pos_fmeasure.append(cv_pos_fmeasure)
+            neg_fmeasure.append(cv_neg_fmeasure)
+            
+            cv_count += 1
+                
+        print('---------------------------------------')
+        print('N-FOLD CROSS VALIDATION RESULT ' + '(' + classifierName + ')')
+        print('---------------------------------------')
+        print('accuracy:', sum(accuracy) / n)
+        print('precision', (sum(pos_precision)/n + sum(neg_precision)/n) / 2)
+        print('recall', (sum(pos_recall)/n + sum(neg_recall)/n) / 2)
+        print('f-measure', (sum(pos_fmeasure)/n + sum(neg_fmeasure)/n) / 2)
+        
+    
+        
+#evaluate_classifier(word_feats)
+#evaluate_classifier(stopword_filtered_word_feats)
+evaluate_classifier(bigram_word_feats)
+#evaluate_classifier(trigram_word_feats)    
+#evaluate_classifier(bigram_word_feats_stopwords)
